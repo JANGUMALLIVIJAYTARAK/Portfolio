6538764d96efe3ce42522d5f104c4489754ed278
vijaytarak <=>
2025-07-14 13:25:38 +0530

    updated Resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -43,7 +43,7 @@ const Hero = () => {
           <motion.div className="hero-buttons" variants={fadeInVariants} custom={4}>
             <a href="#projects" className="btn btn-primary btn-solid">View My Work</a>
             <a href="#contact" className="btn btn-primary btn-solid">Contact Me</a>
-            <a href="images/Tarak's Resume.pdf" download className="btn btn-primary btn-solid">Download Resume</a>
+            <a href="images\Vijay_Tarak_Resume.pdf" download className="btn btn-primary btn-solid">Download Resume</a>
           </motion.div>
         </motion.div>
       </div>
